e0ad00e1e6b5d73e40e42ae515ed1eb9d58ebe9a
takoosh808 <130889840+takoosh808@users.noreply.github.com>
2025-03-16 19:09:41 -0700

    Added rest framework for user authentification

--- myscheduler/myscheduler/settings.py
+++ myscheduler/myscheduler/settings.py
@@ -147,3 +147,10 @@ MEDIA_ROOT = BASE_DIR/"media"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "core.Student"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    # Other settings...
+}
