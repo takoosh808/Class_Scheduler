e0250eed72f0c948868cae19b926cca38b3c064c
takoosh808 <130889840+takoosh808@users.noreply.github.com>
2025-03-16 19:14:18 -0700

    Create ability to register student

--- myscheduler/course_app/views.py
+++ myscheduler/course_app/views.py
@@ -1,19 +1,13 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
-from .models import Course
-from .serializers import CourseSerializer, DetailedCourseSerializer
+from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import StudentSerializer
 
-# Create your views here.
-
-@api_view(["GET"])
-def courses(request):
-    courses = Course.objects.all()
-    serializer = CourseSerializer(courses, many=True)
-    return Response(serializer.data)
-
-@api_view(["GET"])
-def course_detail(request, slug):
-    courses = Course.objects.all(slug=slug)
-    serializer = DetailedCourseSerializer(courses)
-    return Response(serializer.data)
+@api_view(['POST'])
+def register_student(request):
+    if request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
