d5679595e982a4d690923d726ad0509497f735f5
Travis Takushi <travis.takushi@wsu.edu>
2025-04-02 17:03:44 -0700

    Create student model in backend

--- Backend/core/app/api/serializers.py
+++ Backend/core/app/api/serializers.py
@@ -1,8 +1,14 @@
-from .. models import Course
+from .. models import Course, Student
 from rest_framework.serializers import ModelSerializer
 
 class CourseSerializer(ModelSerializer):
     class Meta:
         model = Course
         fields = ('id_number', 'class_name', 'date', 'time')
+
+
+class StudentSerializer(ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ('id_number', 'name', 'password')
         
--- Backend/core/app/models.py
+++ Backend/core/app/models.py
@@ -9,3 +9,11 @@ class Course(models.Model):
 
     def __str__(self):
         return f"Course: {self.class_name}"
+    
+class Student(models.Model):
+    id_number = models.CharField(max_length=8)
+    name = models.CharField(max_length=30)
+    password = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return f"Course: {self.name}"   
