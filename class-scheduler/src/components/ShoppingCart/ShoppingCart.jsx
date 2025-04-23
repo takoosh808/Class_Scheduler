f6d7b34c2b04134c649eda0c4a75589d3a931579
bee katsu <benjamin.k.metzger@wsu.edu>
2025-04-23 16:44:07 -0700

    initial sprint 3 starting point

--- class-scheduler/src/components/ShoppingCart/ShoppingCart.jsx
+++ class-scheduler/src/components/ShoppingCart/ShoppingCart.jsx
@@ -3,6 +3,22 @@ import './ShoppingCart.css'
 
 export default function ShoppingCart() {
   return (
-    <div><h1>Shopping Cart</h1></div>
+    <div id="shopping-container">
+      <h3 style={{color:"var(--button-primary)"}}>Your shopping cart:</h3>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Select</th>
+            <th scope="col">Course ID</th>
+            <th scope="col">Description</th>
+            <th scope="col">Day/Time</th>
+            <th scope="col">Room</th>
+            <th scope="col">Instructor</th>
+            <th scope="col">Credits</th>
+          </tr>
+        </thead>
+
+      </table>
+    </div>
   )
 }
